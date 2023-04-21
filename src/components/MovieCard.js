6766784f9b9f9db9e642f04de9b23d65e45343de
Victor Leina Ngeny <victorleinangeny@gmail.com>
2023-04-21 13:53:11 +0300

    lab completed

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,31 @@
-import React from "react";
+// parent component
+function App() {
+  const title = "Mad Max";
+  const posterURL =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
+  const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
 
-function MovieCard({
-  title,
-  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
-  genres,
-}) {
+  return (
+    <div className="App">
+      {/* passing down props from the parent component */}
+      <MovieCard title={title} posterSrc={posterURL} genres={genresArr} />
+    </div>
+  );
+}
+
+// child component
+// without destructuring
+function MovieCard(props) {
+  return (
+    <div className="movie-card">
+      <img src={props.posterSrc} alt={props.title} />
+      <h2>{props.title}</h2>
+      <small>{props.genres.join(", ")}</small>
+    </div>
+  );
+}
+//with destructuring
+function MovieCard({ title, posterSrc, genres }) {
   return (
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
@@ -13,5 +34,5 @@ function MovieCard({
     </div>
   );
 }
-
 export default MovieCard;
+
